1fd4c5b2493dce7a9b4d2a21ed7d73def88dccf2
mufteevir <mufteevir@gmail.com>
2022-11-14 15:52:16 +0300

    binary search item in list

---
+++ Algorithms/bynary search.py
@@ -0,0 +1,21 @@
+def binary_search(list, item):
+    low = 0
+    high = len(list) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        guess = list[mid]
+        if guess == item:
+            return mid
+        elif guess > item:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return None
+
+
+mylist = [1, 3, 5, 7, 9]
+
+print(binary_search(mylist, 5))
+print(binary_search(mylist, 9))
+print(binary_search(mylist, 1))
+print(binary_search(mylist, 23))
