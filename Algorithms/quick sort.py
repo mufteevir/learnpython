caa601168526c3ba0b555c158c7ceb86e3ca2d60
mufteevir <mufteevir@gmail.com>
2022-11-23 15:47:27 +0300

    sorting using quick sort

--- Algorithms/quick sort.py
+++ Algorithms/quick sort.py
@@ -2,10 +2,10 @@ def quicksort(array):
     if len(array) < 2:
         return array
     else:
-        pivot = array[0]
-        less = [i for i in array[1:] if i <= pivot]
-        greater = [i for i in array[1:] if i > pivot]
-        return quicksort(less) + [pivot] + quicksort(greater)
+        pivot = array[0]#Recursive Case
+        less = [i for i in array[1:] if i <= pivot] #Subarray of all elements,smaller reference
+        greater = [i for i in array[1:] if i > pivot]  #Subarray of all elements,bigger reference
+        return quicksort(less) + [pivot] + quicksort(greater)#recursiion
 
 
 print(quicksort([3, 5, 1, 10, 2]))
